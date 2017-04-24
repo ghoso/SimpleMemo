8a44ec19150e0cb8c8070d0b316bc07552595367
Go Hosohara <go.hosohara@gmail.com>
2017-04-24 19:33:19 +0900

    indexページでメモ表示。

--- app.py
+++ app.py
@@ -3,12 +3,33 @@
 from flask import Flask,render_template,request,redirect,url_for,g
 import sqlite3
 
+DATABASE = 'db/memo.db'
 app= Flask(__name__)
+app.config.from_object(__name__)
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+@app.before_request
+def before_request():
+    
+    con = connect_db()
+    con.row_factory = sqlite3.Row
+    g.db = con
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
 
 # Routing
 @app.route('/')
 def index():
-    return render_template('index.html')
+    con = g.db.cursor()
+    cur = con.execute("select * from memos")
+    rows = cur.fetchall()
+    return render_template('index.html',rows = rows)
 
 @app.route('/new',methods=['POST'])
 def create_memo():
